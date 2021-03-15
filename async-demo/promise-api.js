6808d114201d6a3d51054839a437d43b14aab011
tidusdc <tidusstylez@gmail.com>
2021-03-16 00:03:18 +0100

    Add async practice code.

---
+++ async-demo/promise-api.js
@@ -0,0 +1,18 @@
+const p1 = new Promise( (resolve) => {
+    setTimeout(() => {
+        console.log('Async Operation 1...');
+        resolve(1);
+    }, 2000);
+});
+
+
+const p2 = new Promise( (resolve) => {
+    setTimeout(() => {
+        console.log('Async Operation 2...');
+        resolve(2);
+    }, 2000);
+});
+
+Promise.race([p1,p2])
+.then( result => console.log(result))
+.catch( err => console.log('Error', err.message));
